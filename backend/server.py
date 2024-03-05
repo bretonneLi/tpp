ba453b5119bb30449cdcac9cedb23034b8b76b83
BenhaoDU <dubenhao@gmail.com>
2024-03-05 17:28:09 +0100

    Store the input question in MySQL

--- backend/server.py
+++ backend/server.py
@@ -69,7 +69,8 @@ template="""[INST]
 #connect to MySQL
 
 
-
+def get_connection_sql():
+    return pymysql.connect(host="localhost", port=3306, user="test_user", password="123456", database="test_data", charset="utf8")
 
 
 
@@ -85,7 +86,7 @@ def get_homepage():
 
 @app.get("/database",tags=["初始三个问题"])
 def get_mysql():
-    conn=pymysql.connect(host="localhost", port=3306, user="test_user", password="123456", database="test_data", charset="utf8")
+    conn=get_connection_sql()
     cursor=conn.cursor()
     try:
         cursor.execute("SELECT question ,number_of_question FROM test_question ORDER BY number_of_question DESC;")
@@ -94,7 +95,7 @@ def get_mysql():
         print("error")
     finally:
         cursor.close()
-    return dict(data)
+    return [k for k in dict(data).keys()]
 
 @app.post("/uploadfile/")
 async def get_upload_file(files: List[UploadFile]):
@@ -139,10 +140,19 @@ async def get_retriever(file:UploadFile):#fileinfo:file_info, ,uploaded_time:dat
                 "vector_id":vectorstore.index_to_docstore_id,
                 "vector_path":vectorstore_path,
             }
-
 @app.get("/pdf_retriever/invoke", tags=["语言模型推理接口"])#请求什么时候到达后端
 async def get_response(filename:str,question:str):
     print("Request received")
+    conn=get_connection_sql()
+    cursor=conn.cursor()
+    try:
+        cursor.execute(f"INSERT INTO test_question (question,number_of_question) VALUES (\'{question}\',1);")
+        conn.commit()
+    except Exception :
+        print("Write error")
+    finally:
+        cursor.close()
+        conn.close()
     filestore_path=main_path+f"/pdf_retriever/{filename}"# path
     vectorstore_path=main_path+f"/vectorstore/{filename}"#path
     if os.path.exists(vectorstore_path) and os.path.exists(filestore_path):
@@ -168,7 +178,7 @@ async def delete_file(filename:str):
     #删除储存的向量数据
     vectorstore_path=main_path+f"/vectorstore/{filename}"#path
     if os.path.exists(vectorstore_path):
-        vectorstore=FAISS.load_local(vectorstore_path,embeddings=hf)
+
         os.remove(vectorstore_path+("/index.faiss"))
         os.remove(vectorstore_path+("/index.pkl"))
         os.rmdir(vectorstore_path)
@@ -176,7 +186,7 @@ async def delete_file(filename:str):
     filestore_path=main_path+f"/pdf_retriever/{filename}"# path
     if os.path.exists(filestore_path):
         os.remove(filestore_path)
-    return {"delete":filename,"vector_id":vectorstore.index_to_docstore_id}
+    return {"delete":filename}
 
 
 if __name__ == "__main__":
