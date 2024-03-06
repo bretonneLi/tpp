3bbe6ceb8a2cb7947f16e6cb5643a9c5e8dbf80a
BenhaoDU <dubenhao@gmail.com>
2024-03-06 09:09:20 +0100

    all

--- frontend/src/api/chat.js
+++ frontend/src/api/chat.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import Request from "./request";
 
 export function retriver(question){
-    let filename='';
+    let filename='tingyimiao.pdf';
     // 使用模板字符串将参数拼接在 URL 上
     return axios.get(process.env.REACT_APP_TPP_BACK_BASE+`pdf_retriever/invoke?question=${question}&filename=${filename}`);
 }
 
 // 机器人初始加载欢迎语
 export function chatInit(){
-    return axios.get(process.env.REACT_APP_TPP_BACK_BASE+ `chat/init`);
+    return axios.get(process.env.REACT_APP_TPP_BACK_BASE+ `database`);
 }
